d1480c95f14e4b324aaec418a2ff77c35a1289f0
EXPLODE\rikukazu <sakakaze@hotmail.com>
2017-09-04 02:58:28 +0900

    機能分け、挨拶追加、天気と気温追加

--- maidsan/test/softalk/SoftalkTest/SoftalkTest/Program.cs
+++ maidsan/test/softalk/SoftalkTest/SoftalkTest/Program.cs
@@ -14,79 +14,24 @@ namespace SoftalkTest
     {
         static void Main(string[] args)
         {
+            //挨拶
+            var ai = Talk.Aisatu();
+            Talk.Say(ai);
+
+            //天気
+            var we = Weather.GetWeatherText();
+            Talk.Say(we);
+            Talk.Close();
+
+            //talk
             while (true)
             {
                 Console.WriteLine("何かひらがなで入力してください...");
-                string koe = Console.ReadLine();
+                string line = Console.ReadLine();
 
-                //Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
-                //StreamWriter writer = new StreamWriter(@"test.txt", true, sjisEnc);
-                //writer.WriteLine("テスト書き込みです。");
-                //writer.Close();
-
-                // パラメータを指定して実行
-                Process.Start("softalk\\Softalk.exe", "/W:" + koe);
-                Process.Start("softalk\\Softalk.exe", @"/play");
-                Process.Start("softalk\\Softalk.exe", @"/close");
-                
-                //var startInfo = new ProcessStartInfo();
-                //startInfo.FileName = "softalk\\Softalk.exe";
-                //startInfo.CreateNoWindow = true;
-                //startInfo.UseShellExecute = false;
-                //startInfo.Arguments = string.Format(@"/W:{0}", koe);
-                //Process process = Process.Start(startInfo);
-                //Process.Start("softalk\\Softalk.exe", @"/play");
-                //Process.Start("softalk\\Softalk.exe", @"/close");
+                Talk.Say(line + "、ですか？意味が分かりません。");
+                Talk.Close();
             }
         }
     }
-
-
-
-#if false
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-                const int speed = 100;
-                int size = 0;
-
-                Console.WriteLine("何かひらがなで入力してください...");
-                string koe = Console.ReadLine();
-
-                //音声ファイルとしてそのまま保存可能なバイト列の先頭ポイントを取得
-                IntPtr wavPtr = AquesTalk_Synthe(koe, speed, ref size);
-
-                //成功判定
-                if (wavPtr == IntPtr.Zero)
-                {
-                    Console.WriteLine("ERROR: 音声生成に失敗しました。不正な文字が使われた可能性があります");
-                    continue;
-                }
-
-                //C#で扱えるようにマネージド側へコピー
-                byte[] wav = new byte[size];
-                Marshal.Copy(wavPtr, wav, 0, size);
-
-                //アンマネージドポインタは用が無くなった瞬間に解放
-                AquesTalk_FreeWave(wavPtr);
-
-                //そのまま再生
-                using (var ms = new MemoryStream(wav))
-                using (var sp = new SoundPlayer(ms))
-                {
-                    sp.Play();
-                }
-            }
-        }
-
-        const string dllName = "dll\\AquesTalk.dll";
-
-        [DllImport(dllName)]
-        extern static IntPtr AquesTalk_Synthe(string koe, int speed, ref int size);
-
-        [DllImport(dllName)]
-        extern static void AquesTalk_FreeWave(IntPtr wavPtr);
-    }
-#endif
-            }
+}
